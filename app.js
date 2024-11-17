ec10b45b1575c6e90fb1532820469b72960ff64e
Haungz <news.word.game@gmail.com>
2024-11-17 17:31:50 -0300

    Versão com alerta que mostra a quantidade de tentativas

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 alert("Bem vindo ao jogo do número secreto!")
 let numeroSecreto = 8
 let chute = 0
+let tentativas  = 0
 
 while (chute != numeroSecreto){
     chute = prompt("Escolha um número entre 1 e 10")
+    tentativas ++
     if (chute == numeroSecreto) {
         alert(`Isso aí! Você acertou o número secreto!: ${numeroSecreto}`)
     } else if (chute > numeroSecreto) {
@@ -13,3 +15,4 @@ while (chute != numeroSecreto){
     }
 }
 
+alert(`Você acertou o número secreto "${numeroSecreto}" com ${tentativas} tentativas`)
